0abdf62f706840fe1280d23ba512b0fb627101d1
Aris Blevins <aris@goteama.com>
2014-12-15 21:50:59 -0800

    2nd route test case
    Signed-off-by: Aris Blevins <aris@goteama.com>

--- scenario2/assets/js/app-base.js
+++ scenario2/assets/js/app-base.js
@@ -9,6 +9,10 @@ app.config( function( $routeProvider ) {
 		templateUrl: '01-media-grid.html',
 		controller: 'mediaGridCtrl',
 	});
+	$routeProvider.when( '/source', {
+		templateUrl: '02-choose-source.html',
+		controller: 'sourceCtrl',
+	});
 })
 
 app.service('currentImage', function() {
@@ -18,6 +22,54 @@ app.service('currentImage', function() {
 app.controller( 'mediaGridCtrl', ['$rootScope', '$scope', 'currentImage', '$http', function( $rootScope, $scope, currentImage, $http ) {
 	/** OPEN MODAL **/
 	$('#media-modal').foundation('reveal', 'open' );
+	
+	$scope.selectedImages = currentImage,
+		
+	$http.get('assets/js/data.json').then(function(res){
+		$scope.images = res.data.images
+	});
+	
+	$scope.imageSelect = function(key, e) {
+		if( e.target.localName == 'img' || $(e.target).hasClass('image-wrap') ){
+			$(e.target).parents('div.grid-wrap').toggleClass('selected');
+		} else {
+			$(e.target).toggleClass('selected');
+		}
+		$scope.showFooter();
+	}
+	
+	$scope.deSelect = function(){
+		$('div.grid-wrap.selected').removeClass('selected');
+		$scope.showFooter();
+	}
+	
+	$scope.showFooter = function() {
+		var m = $('div.grid-wrap.selected').length;
+		
+		if( m > 0 ) {
+			$('#bottom-toolbar').fadeIn();
+			$('#selected-count').text('');
+	      	$('#selected-items').text(m + ' Selected');
+	      	$('#selected-count').text('(' + m + ')');
+		} else {
+			$('#bottom-toolbar').fadeOut();
+			$('#selected-items').text('');
+		}
+		
+	}
+	
+	$scope.insertImages = function() {
+		console.log( $scope.selectedImages );
+		
+		$.each( $('div.grid-wrap.selected'), function( value, key ) {
+			$scope.selectedImages.images.push( $scope.images[$(this).data('index')] );
+		});
+		
+		console.log( $scope.selectedImages );
+	}
+	
+}]);
+app.controller( 'sourceCtrl', ['$rootScope', '$scope', 'currentImage', '$http', function( $rootScope, $scope, currentImage, $http ) {
 	
 	$scope.selectedImages = currentImage,
 		
