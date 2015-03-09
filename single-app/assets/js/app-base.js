f02f39161f57c42f42d2ff043144cbaaf1017670
Ayman Al Zarrad <aymanalzarrad@gmail.com>
2015-03-09 16:07:28 +0100

    Empty array on insertImages from the full width view

--- single-app/assets/js/app-base.js
+++ single-app/assets/js/app-base.js
@@ -1,53 +1,53 @@
 var app = angular.module('imageflow', [ 'ngRoute' ]);
 
 app.run( function() {
-	
+
 });
 
 /** ROUTES **/
 app.config( function( $routeProvider ) {
-	$routeProvider.when( '/mediaGrid', {
-		templateUrl: '01-media-grid.html',
-		controller: 'mediaGridCtrl',
-	});
-	$routeProvider.when( '/source', {
-		templateUrl: '02-choose-source.html',
-		controller: 'sourceCtrl',
-	});
-	$routeProvider.when( '/select', {
-		templateUrl: '03-upload-image.html',
-		controller: 'sourceCtrl',
-	});
-	$routeProvider.when( '/uploading', {
-		templateUrl: '04-media-grid-uploading.html',
-		controller: 'uploadingCtrl',
-	});
-	$routeProvider.when( '/fullWidth', {
-		templateUrl: '05-full-width.html',
-		controller: 'fullWidthCtrl',
-	});
+    $routeProvider.when( '/mediaGrid', {
+        templateUrl: '01-media-grid.html',
+        controller: 'mediaGridCtrl',
+    });
+    $routeProvider.when( '/source', {
+        templateUrl: '02-choose-source.html',
+        controller: 'sourceCtrl',
+    });
+    $routeProvider.when( '/select', {
+        templateUrl: '03-upload-image.html',
+        controller: 'sourceCtrl',
+    });
+    $routeProvider.when( '/uploading', {
+        templateUrl: '04-media-grid-uploading.html',
+        controller: 'uploadingCtrl',
+    });
+    $routeProvider.when( '/fullWidth', {
+        templateUrl: '05-full-width.html',
+        controller: 'fullWidthCtrl',
+    });
 })
 
 /** GLOBAL VAR FOR CURRENT IMAGES **/
 app.service('currentImage', function() {
-	this.images = [];
+    this.images = [];
 })
 
 app.service('shared', function(){
-	this.soon = function(){
-		 alert('Coming Soon!');
-	}
-	
-	this.insertImages = function() {
-		$location.path('/');
-		$('#media-modal').foundation('reveal', 'close' );
-		$('body').trigger( 'insertImages' );
-	}
-	
-	this.selected = [];
-	this.selectImage = function(image){
-		selected.push(image);
-	}
+    this.soon = function(){
+        alert('Coming Soon!');
+    }
+
+    this.insertImages = function() {
+        $location.path('/');
+        $('#media-modal').foundation('reveal', 'close' );
+        $('body').trigger( 'insertImages' );
+    }
+
+    this.selected = [];
+    this.selectImage = function(image){
+        selected.push(image);
+    }
 })
 
 /**
@@ -83,7 +83,6 @@ app.directive('imageWatcher',['currentImage',
         }
     }
 ]);
-
 
 app.controller( 'mediaGridCtrl', ['$rootScope', '$scope', 'currentImage', 'shared', '$http', '$location', function( $rootScope, $scope, currentImage, shared, $http, $location ) {
 
@@ -207,31 +206,29 @@ app.controller( 'mediaGridCtrl', ['$rootScope', '$scope', 'currentImage', 'share
     );
 
 }]);
-
-
 app.controller( 'textAreaCtrl', ['$rootScope', '$scope', 'currentImage', 'shared', '$http', '$location', function( $rootScope, $scope, currentImage, shared, $http, $location ) {
-	
-	$('body').on('insertImages', function() {
-		$.each( currentImage.images, function( key, value ) {
-			$scope.curVal = $('#tinyMCE').html();
-			$scope.newVal = $scope.curVal + '<img src="' + value + '" />';
-			$('#tinyMCE').html( $scope.newVal );
-		});
-	});
-	
+
+    $('body').on('insertImages', function() {
+        $.each( currentImage.images, function( key, value ) {
+            $scope.curVal = $('#tinyMCE').html();
+            $scope.newVal = $scope.curVal + '<img src="' + value + '" />';
+            $('#tinyMCE').html( $scope.newVal );
+        });
+    });
+
 }]);
 app.controller( 'sourceCtrl', ['$rootScope', '$scope', 'currentImage', 'shared', '$http', '$location', function( $rootScope, $scope, currentImage, shared, $http, $location ) {
 
-	//$rootScope.secondtime = 1;
-    
-	$scope.modalTitle = 'Upload File';
-	
-	$scope.goHome = function(){
-		$location.path('/');
+    //$rootScope.secondtime = 1;
+
+    $scope.modalTitle = 'Upload File';
+
+    $scope.goHome = function(){
+        $location.path('/');
         /* Here we should empty the array */
         $scope.selectedImages.images = [];
-	}
-	
+    }
+
 }]);
 app.controller( 'uploadingCtrl', ['$rootScope', '$scope', 'currentImage', 'shared', '$http', '$location', function( $rootScope, $scope, currentImage, shared, $http, $location ) {
 
@@ -341,134 +338,134 @@ app.controller( 'uploadingCtrl', ['$rootScope', '$scope', 'currentImage', 'share
 app.directive('onLastRepeat', function() {
     return function(scope, element, attrs) {
         if (scope.$last) setTimeout(function(){
-        scope.$emit('onRepeatLast', element, attrs);
+            scope.$emit('onRepeatLast', element, attrs);
         }, 1);
     };
 });
 
 app.controller( 'fullWidthCtrl', ['$rootScope', '$scope', 'currentImage', 'shared', '$http', '$location', function( $rootScope, $scope, currentImage, shared, $http, $location ) {
-    
-	//$('#media-modal').foundation('reveal', 'open' );	
-	//$rootScope.secondtime = 1;
-    
-	$scope.modalTitle = 'Select Media';
-	$scope.selectedImages = currentImage;
-	//$scope.selectedImages.images = [];
-    
-	$scope.$on('$viewContentLoaded', function() {
-	    $('#grid_menu').css("display","none");  
-	    $('#full_width_menu').css("display","block");      
-	});
 
-	// Start Position
-	$scope.$on('onRepeatLast', function(scope, element, attrs){
-        
-		//Size adjust
-		mediamodalheight = parseInt( $('#media-modal').css("height") );
-		aux = mediamodalheight-132-40; 
-		
-		positioncount= 0;
-		
-		$('div.fullwidth-wrap img').each(function(index, value) { 
-		    var obj = $(this).parents('div.card-container');
-		    var selector = $(this).parents('div.fullwidth-wrap');
-		    if($.inArray($(this).attr('src'),currentImage.images)>-1){
-			selector.toggleClass('selected');
-			$(this).siblings('i.fa').css("display","block");
-			var m = currentImage.images.length;
-			if( m > 0 ) {
-			    $('#bottom-toolbar').fadeIn();
-			    $('#selected-count').text('');
-			    if (m > 1) {
-				$('#selected-items').text(m + ' Selected');
-				$('#selected-count').text('(' + m + ')');
-			    }
-			}                 
-		    } else {
-			$(this).siblings('i.fa').css("display","block");              
-		    }
-		    obj.css( "position", "absolute" ); 
-		    obj.css( "top", 12 );
-		    obj.css( "left", positioncount );
+    //$('#media-modal').foundation('reveal', 'open' );
+    //$rootScope.secondtime = 1;
 
-		    positioncount = positioncount+1000;
-	
-			
-		    //Size adjust
-		   /* $(this).css("max-height",aux); 
-		    if($(this).height() < aux ) {
-			aux2 = (aux - $(this).height() ) / 2 ;
-			$(this).css("top", aux2);   
-		    }*/            
-		    
-		});
-		
-		//Size adjust
-	
-		$('.fw-image-wrap').css("height",aux);
-		$('.fullwidth-wrap').css("height",aux);
-		$('div.metawrap div.meta').css("height",aux);
-		$('.meta-wrap').css("height",aux);
-		$('#cg-button').css("display", "inline");
-		$('#fw-button').css("display", "none");       
+    $scope.modalTitle = 'Select Media';
+    $scope.selectedImages = currentImage;
+    //$scope.selectedImages.images = [];
 
-        var h = $('div.fullwidth-wrap').length;    
-        $('#totalimg').text(h);         
-        
-	});
-    
-	// Click RIGHT
-    
-	function moveright() {
-	    if (( parseInt( $('div.card-container').last().css("left") ) > 0 ) && ( parseInt($('div.card-container').first().css("left"))%1000 === 0) ) { 
-		$('div.card-container').each(function(index, value) {
-		    var obj2 = $(this);
-		    obj2.addClass("transi");
-		    leftposition = $(this).css("left");
-		    var leftposition = parseInt($(this).css("left")) -1000;
-		    $(this).css("left", leftposition);
-		});
-		myInteger = parseInt($('#actualimg').html());
-		$('#actualimg').text((myInteger+1));         
-	    }
-	}
-    
-	$('a.right-arrow').click(function() {      
-		moveright();
-	}); 
-	   
-	$("#full-width-area").on("swipeleft",function(){
-		moveright();
-	});
-	
-	//Click LEFT
+    $scope.$on('$viewContentLoaded', function() {
+        $('#grid_menu').css("display","none");
+        $('#full_width_menu').css("display","block");
+    });
 
-	function moveleft() {      
-	    if (( parseInt($('div.card-container').first().css("left")) < 0 )
-	    && (parseInt($('div.card-container').first().css("left"))%1000 === 0)) { 
+    // Start Position
+    $scope.$on('onRepeatLast', function(scope, element, attrs){
+
+        //Size adjust
+        mediamodalheight = parseInt( $('#media-modal').css("height") );
+        aux = mediamodalheight-132-40;
+
+        positioncount= 0;
+
+        $('div.fullwidth-wrap img').each(function(index, value) {
+            var obj = $(this).parents('div.card-container');
+            var selector = $(this).parents('div.fullwidth-wrap');
+            if($.inArray($(this).attr('src'),currentImage.images)>-1){
+                selector.toggleClass('selected');
+                $(this).siblings('i.fa').css("display","block");
+                var m = currentImage.images.length;
+                if( m > 0 ) {
+                    $('#bottom-toolbar').fadeIn();
+                    $('#selected-count').text('');
+                    if (m > 1) {
+                        $('#selected-items').text(m + ' Selected');
+                        $('#selected-count').text('(' + m + ')');
+                    }
+                }
+            } else {
+                $(this).siblings('i.fa').css("display","block");
+            }
+            obj.css( "position", "absolute" );
+            obj.css( "top", 12 );
+            obj.css( "left", positioncount );
+
+            positioncount = positioncount+1000;
+
+
+            //Size adjust
+            /* $(this).css("max-height",aux);
+             if($(this).height() < aux ) {
+             aux2 = (aux - $(this).height() ) / 2 ;
+             $(this).css("top", aux2);
+             }*/
+
+        });
+
+        //Size adjust
+
+        $('.fw-image-wrap').css("height",aux);
+        $('.fullwidth-wrap').css("height",aux);
+        $('div.metawrap div.meta').css("height",aux);
+        $('.meta-wrap').css("height",aux);
+        $('#cg-button').css("display", "inline");
+        $('#fw-button').css("display", "none");
+
+        var h = $('div.fullwidth-wrap').length;
+        $('#totalimg').text(h);
+
+    });
+
+    // Click RIGHT
+
+    function moveright() {
+        if (( parseInt( $('div.card-container').last().css("left") ) > 0 ) && ( parseInt($('div.card-container').first().css("left"))%1000 === 0) ) {
             $('div.card-container').each(function(index, value) {
-                var obj2 = $(this); 
-                obj2.addClass("transi"); 
+                var obj2 = $(this);
+                obj2.addClass("transi");
                 leftposition = $(this).css("left");
-                var leftposition = parseInt($(this).css("left")) +1000;
-                $(this).css("left", leftposition);   
+                var leftposition = parseInt($(this).css("left")) -1000;
+                $(this).css("left", leftposition);
             });
             myInteger = parseInt($('#actualimg').html());
-            $('#actualimg').text((myInteger-1));              
-	    }
-	}    
-    
-	$('a.left-arrow').click(function() {
-		moveleft();        
-	}); 
+            $('#actualimg').text((myInteger+1));
+        }
+    }
 
-	$("#full-width-area").on("swiperight",function(){
-		moveleft();
-	});    
-    
-	$http.get('assets/js/data.json').then(function(res){
-		$scope.images = res.data.images;
-	});
+    $('a.right-arrow').click(function() {
+        moveright();
+    });
+
+    $("#full-width-area").on("swipeleft",function(){
+        moveright();
+    });
+
+    //Click LEFT
+
+    function moveleft() {
+        if (( parseInt($('div.card-container').first().css("left")) < 0 )
+            && (parseInt($('div.card-container').first().css("left"))%1000 === 0)) {
+            $('div.card-container').each(function(index, value) {
+                var obj2 = $(this);
+                obj2.addClass("transi");
+                leftposition = $(this).css("left");
+                var leftposition = parseInt($(this).css("left")) +1000;
+                $(this).css("left", leftposition);
+            });
+            myInteger = parseInt($('#actualimg').html());
+            $('#actualimg').text((myInteger-1));
+        }
+    }
+
+    $('a.left-arrow').click(function() {
+        moveleft();
+    });
+
+    $("#full-width-area").on("swiperight",function(){
+        moveleft();
+    });
+
+    $http.get('assets/js/data.json').then(function(res){
+        $scope.images = res.data.images;
+    });
 
 
     $scope.imageSelect = function(index, event) {
@@ -502,62 +499,64 @@ app.controller( 'fullWidthCtrl', ['$rootScope', '$scope', 'currentImage', 'share
 
         $scope.showFooter();
     }
-	
-	$scope.deSelect = function(){
-        /*$('div.fullwidth-wrap.selected i.fa').toggle();*/       
-		$('div.fullwidth-wrap.selected').removeClass('selected');
-		$scope.selectedImages.images = [];
-		$scope.showFooter();
-	}
-	
-	$scope.showFooter = function() {
-		var m = $('div.fullwidth-wrap.selected').length;
-		
-		if( m > 0 ) {
-			$('#bottom-toolbar').fadeIn();
-			$('#selected-count').text('');
-			$('#selected-items').text('');            
-			if (m > 1) {
-				$('#selected-items').text(m + ' Selected');
-				$('#selected-count').text('(' + m + ')');
-			}
-		} else {
-			$('#bottom-toolbar').fadeOut();
-			$('#selected-items').text('');
-		}
-		
-	}
-    
-	$scope.insertImages = function() {
-		$location.path('/');
-		$('#media-modal').foundation('reveal', 'close' );
-		$('body').trigger( 'insertImages' );
-	}
-	
-	$scope.goHome = function(){
-		$location.path('/');
+
+    $scope.deSelect = function(){
+        /*$('div.fullwidth-wrap.selected i.fa').toggle();*/
+        $('div.fullwidth-wrap.selected').removeClass('selected');
+        $scope.selectedImages.images = [];
+        $scope.showFooter();
+    }
+
+    $scope.showFooter = function() {
+        var m = $('div.fullwidth-wrap.selected').length;
+
+        if( m > 0 ) {
+            $('#bottom-toolbar').fadeIn();
+            $('#selected-count').text('');
+            $('#selected-items').text('');
+            if (m > 1) {
+                $('#selected-items').text(m + ' Selected');
+                $('#selected-count').text('(' + m + ')');
+            }
+        } else {
+            $('#bottom-toolbar').fadeOut();
+            $('#selected-items').text('');
+        }
+
+    }
+
+    $scope.insertImages = function() {
+        $location.path('/');
+        $('#media-modal').foundation('reveal', 'close' );
+        $('body').trigger( 'insertImages' );
         /* Here we should empty the array */
         $scope.selectedImages.images = [];
-	}
-	
-	$scope.soon = function(){
-		shared.soon();
-	}
+    }
 
-	$scope.flip = function(e){
-		var obj = $(e.target).parents('div#meta-data-view');
-		if (obj.hasClass('toggled')) {
-			$(".flipper").velocity('reverse');
-			obj.removeClass('toggled');
-		} else {
-			obj.addClass('toggled');
-			$(".flipper").velocity({ rotateY: "180deg"}, 1000);
-		}	
-	}
-	
-	/*$scope.gobackgrid = function() {
-        $scope.saveselectedimages = 1 ;
-        window.location.href = "http://imageflow.pabloperea.com/single-app/#/mediaGrid";
-	}*/
-    
+    $scope.goHome = function(){
+        $location.path('/');
+        /* Here we should empty the array */
+        $scope.selectedImages.images = [];
+    }
+
+    $scope.soon = function(){
+        shared.soon();
+    }
+
+    $scope.flip = function(e){
+        var obj = $(e.target).parents('div#meta-data-view');
+        if (obj.hasClass('toggled')) {
+            $(".flipper").velocity('reverse');
+            obj.removeClass('toggled');
+        } else {
+            obj.addClass('toggled');
+            $(".flipper").velocity({ rotateY: "180deg"}, 1000);
+        }
+    }
+
+    /*$scope.gobackgrid = function() {
+     $scope.saveselectedimages = 1 ;
+     window.location.href = "http://imageflow.pabloperea.com/single-app/#/mediaGrid";
+     }*/
+
 }]);
